819c59c0845e89d7fedc103378b872f8347a7399
JiangyueZhao <91275468+JiangyueZhao@users.noreply.github.com>
2020-08-29 15:15:55 +0000

    Remove unused header.

--- app/src/main/java/me/aprilian/coinbasesample/MainActivity.kt
+++ app/src/main/java/me/aprilian/coinbasesample/MainActivity.kt
@@ -20,4 +20,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        if (savedInstanceState ==
+        if (savedInstanceState == null) setupBottomNavigationBar()
+    }
+
+    private 
