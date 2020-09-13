f3a987431aba7c65b734faa5d1637e416dc4e0be
JiangyueZhao <91275468+JiangyueZhao@users.noreply.github.com>
2020-09-13 12:45:14 +0000

    ReadMe

--- app/src/main/java/me/aprilian/coinbasesample/api/CoinApiService.kt
+++ app/src/main/java/me/aprilian/coinbasesample/api/CoinApiService.kt
@@ -2,4 +2,8 @@ package me.aprilian.coinbasesample.api
 
 import me.aprilian.coinbasesample.data.LatestCoinResponse
 import retrofit2.Response
-import retro
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface CoinApiService {
+    @GE
