cb286876e0c06623ac667121bd2590454d8a5584
JiangyueZhao <91275468+JiangyueZhao@users.noreply.github.com>
2020-10-25 19:12:14 +0000

    Small text changes

--- app/src/main/java/me/aprilian/coinbasesample/data/LatestCoinResponse.kt
+++ app/src/main/java/me/aprilian/coinbasesample/data/LatestCoinResponse.kt
@@ -29,4 +29,5 @@ data class LatestCoinResponse(
         @SerializedName("num_market_pairs")
         val numMarketPairs: Int?,
         @SerializedName("quote")
-  
+        val quote: Quote?,
+        @SerializedName(
