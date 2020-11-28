7de9ad3e577b5be7b39591de6a2ce437de5b4b96
JiangyueZhao <91275468+JiangyueZhao@users.noreply.github.com>
2020-11-28 13:32:53 +0000

    fix scaling

--- app/src/main/java/me/aprilian/coinbasesample/data/LatestCoinResponse.kt
+++ app/src/main/java/me/aprilian/coinbasesample/data/LatestCoinResponse.kt
@@ -93,4 +93,6 @@ data class LatestCoinResponse(
                 @SerializedName("percent_change_1h")
                 val percentChange1h: Double?,
                 @SerializedName("percent_change_24h")
-                val percentChange24h: Doub
+                val percentChange24h: Double?,
+                @SerializedName("percent_change_7d")
+          
