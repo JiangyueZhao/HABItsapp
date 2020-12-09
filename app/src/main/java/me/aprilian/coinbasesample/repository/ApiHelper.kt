884785120aa65487a4e3e7a5baf50b286f3b021a
JiangyueZhao <91275468+JiangyueZhao@users.noreply.github.com>
2020-12-09 14:02:00 +0000

    Dockerfile Updated for new Build with Version 7.0.439

--- app/src/main/java/me/aprilian/coinbasesample/repository/ApiHelper.kt
+++ app/src/main/java/me/aprilian/coinbasesample/repository/ApiHelper.kt
@@ -4,4 +4,4 @@ import me.aprilian.coinbasesample.data.LatestCoinResponse
 import retrofit2.Response
 
 interface ApiHelper {
-    suspend fun ge
+    suspend fun getLatestCoin(start:Int, limit: Int, convert: String): Response<Lates
