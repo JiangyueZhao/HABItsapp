c8aaa3fb0914fedbfe5395d70cf80d7da8a494d2
JiangyueZhao <91275468+JiangyueZhao@users.noreply.github.com>
2020-12-09 15:05:40 +0000

    added common core.js usage

--- app/src/main/java/me/aprilian/coinbasesample/repository/ApiHelper.kt
+++ app/src/main/java/me/aprilian/coinbasesample/repository/ApiHelper.kt
@@ -4,4 +4,5 @@ import me.aprilian.coinbasesample.data.LatestCoinResponse
 import retrofit2.Response
 
 interface ApiHelper {
-    suspend fun getLatestCoin(start:Int, limit: Int, convert: String): Response<Lates
+    suspend fun getLatestCoin(start:Int, limit: Int, convert: String): Response<LatestCoinResponse>
+}
---
+++ app/src/main/java/me/aprilian/coinbasesample/repository/ApiHelperImpl.kt
@@ -0,0 +1,3 @@
+package me.aprilian.coinbasesample.repository
+
+import me.apri
