bf553cae9d81ccb1a7a25885ac609fe5653770cd
JiangyueZhao <91275468+JiangyueZhao@users.noreply.github.com>
2020-12-10 13:33:34 +0000

    added timer

--- app/src/main/java/me/aprilian/coinbasesample/repository/ApiHelperImpl.kt
+++ app/src/main/java/me/aprilian/coinbasesample/repository/ApiHelperImpl.kt
@@ -1,4 +1,5 @@
 package me.aprilian.coinbasesample.repository
 
 import me.aprilian.coinbasesample.api.CoinApiService
-import me.aprilian.co
+import me.aprilian.coinbasesample.data.LatestCoinResponse
+import retrofit2.Respons
