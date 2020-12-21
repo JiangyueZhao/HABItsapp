bd0a5e2d78eed453e401a20f58af80a21edd7957
JiangyueZhao <91275468+JiangyueZhao@users.noreply.github.com>
2020-12-21 19:21:05 +0000

    m32r: add memcpy() for CONFIG_KERNEL_GZIP=y

--- app/src/main/java/me/aprilian/coinbasesample/ui/CoinDetailFragment.kt
+++ app/src/main/java/me/aprilian/coinbasesample/ui/CoinDetailFragment.kt
@@ -5,4 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import me.aprilian.coinbasesample.dat
+import me.aprilian.coinbasesample.databinding.FragmentCoinDetailBinding
+
+class CoinDetailFragment
