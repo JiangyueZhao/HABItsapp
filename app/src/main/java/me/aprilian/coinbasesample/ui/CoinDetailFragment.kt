5459d16c3149c55ac4d742e5e328a2032b5e4b4a
JiangyueZhao <91275468+JiangyueZhao@users.noreply.github.com>
2020-12-22 06:02:52 +0000

    more test updates

--- app/src/main/java/me/aprilian/coinbasesample/ui/CoinDetailFragment.kt
+++ app/src/main/java/me/aprilian/coinbasesample/ui/CoinDetailFragment.kt
@@ -11,4 +11,5 @@ class CoinDetailFragment : Fragment() {
 
     private lateinit var binding: FragmentCoinDetailBinding
 
-    override fun onCreateView(inflater: LayoutInflater, container:
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        bind
