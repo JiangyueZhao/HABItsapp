f51c4b517396c9e52ad02f6dc59f1c92d2b4cd07
JiangyueZhao <91275468+JiangyueZhao@users.noreply.github.com>
2020-12-28 08:32:22 +0000

    Fix indentation issues

--- app/src/main/java/me/aprilian/coinbasesample/ui/SettingsFragment.kt
+++ app/src/main/java/me/aprilian/coinbasesample/ui/SettingsFragment.kt
@@ -4,4 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.Surface
 import android.view.View
-import android.view.ViewGro
+import android.view.ViewGroup
+import androidx.compose.runtime.Composable
+import androidx.co
