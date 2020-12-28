efa0a91763f30cb4337578930a09b64b95266324
JiangyueZhao <91275468+JiangyueZhao@users.noreply.github.com>
2020-12-28 08:01:38 +0000

    crypto: af_alg - Fix socket double-free when accept fails

--- app/src/main/java/me/aprilian/coinbasesample/ui/SettingsFragment.kt
+++ app/src/main/java/me/aprilian/coinbasesample/ui/SettingsFragment.kt
@@ -2,4 +2,6 @@ package me.aprilian.coinbasesample.ui
 
 import android.os.Bundle
 import android.view.LayoutInflater
-import andro
+import android.view.Surface
+import android.view.View
+import android.view.ViewGro
