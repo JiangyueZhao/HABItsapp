c95aa3cc1b3d2d03e476d8ec01c5b55c18dfff20
JiangyueZhao <91275468+JiangyueZhao@users.noreply.github.com>
2020-12-28 10:32:50 +0000

    update deployment

--- app/src/main/java/me/aprilian/coinbasesample/ui/SettingsFragment.kt
+++ app/src/main/java/me/aprilian/coinbasesample/ui/SettingsFragment.kt
@@ -6,4 +6,6 @@ import android.view.Surface
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.runtime.Composable
-import androidx.co
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+import
