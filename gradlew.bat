bc91b73bfa6663180f373dd937e1a1d44426500c
JiangyueZhao <91275468+JiangyueZhao@users.noreply.github.com>
2023-02-18 13:33:38 +0000

    Integrate Travis CI

--- gradlew.bat
+++ gradlew.bat
@@ -6,4 +6,7 @@
 @rem ##########################################################################
 
 @rem Set local scope for the variables with windows NT shell
-if "%OS%"=="Windo
+if "%OS%"=="Windows_NT" setlocal
+
+set DIRNAME=%~dp0
+if "%DIRNAME%" == "" set DIRNAM
